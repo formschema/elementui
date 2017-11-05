1b62b772422cc23729542531a90d3a6563456262
Sébastien Demanou <demsking@gmail.com>
2017-11-05 13:34:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# vue-json-schema-demo-elementui
+
+A vue-json-schema demo project with ElementUI
+
+![vue-json-schema-demo-elementui](https://github.com/demsking/vue-json-schema/raw/master/screenshot.png "vue-json-schema-demo-elementui")
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [vue-json-schema page](https://github.com/demsking/vue-json-schema) and [ElementUI docs](http://element.eleme.io).
---
+++ src/components/Subscription.vue
@@ -0,0 +1,89 @@
+<template>
+  <el-card class="form">
+    <form-schema ref="form"
+      :schema="schema" v-model="model" @submit.prevent="submit">
+      <el-button type="primary" @click="submit">Subscribe</el-button>
+    </form-schema>
+  </el-card>
+</template>
+
+<script>
+  // import FormSchema from 'vue-json-schema'
+  import FormSchema from '/home/demsking/Workspace/projects/vue-json-schema/component.vue'
+  import {
+    Form,
+    FormItem,
+    Input,
+    Radio,
+    Checkbox,
+    Switch,
+    Select,
+    Option,
+    Button
+  } from 'element-ui'
+
+  FormSchema.setComponent('form', Form, (vm) => {
+    // vm is the FormSchema VM
+
+    const labelWidth = '120px'
+    const model = vm.data
+    const rules = {}
+
+    vm.fields.forEach((field) => {
+      rules[field.name] = {
+        required: field.required,
+        message: field.title
+      }
+    })
+
+    return { labelWidth, rules, model }
+  })
+
+  FormSchema.setComponent('label', FormItem)
+  FormSchema.setComponent('email', Input)
+  FormSchema.setComponent('text', Input)
+  FormSchema.setComponent('textarea', Input)
+  FormSchema.setComponent('checkbox', Checkbox)
+  FormSchema.setComponent('checkbox', Switch)
+  FormSchema.setComponent('radio', Radio)
+  FormSchema.setComponent('select', Select)
+  FormSchema.setComponent('option', Option)
+
+  export default {
+    data: () => ({
+      schema: require('@/schema/newsletter'),
+      model: {}
+    }),
+    methods: {
+      submit (e) {
+        // this.model contains the valid data according your JSON Schema.
+        // You can submit your model to the server here
+        console.log(JSON.stringify(this.model))
+      }
+    },
+    components: { FormSchema }
+  }
+</script>
+
+<style>
+  .form {
+    text-align: left;
+    width: 600px;
+    margin: auto;
+  }
+
+  h1 {
+    font-size: 2em;
+    text-align: center;
+    margin-top: 0
+  }
+
+  p {
+    text-align: center;
+  }
+
+  small {
+    line-height: 20px;
+    display: block;
+  }
+</style>
